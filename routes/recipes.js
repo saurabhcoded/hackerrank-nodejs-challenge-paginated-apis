3c8fa21234beb873cd4a2bd3491454e7ce916f9b
Admin <admin.push@hackerrank.workspace>
2023-01-19 05:35:31 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,5 +1,9 @@
 var recipes = require('../recipes.json');
 var router = require('express').Router();
 
+router.get("/recipes",(req,res)=>{
+  console.log("req,res")
+})
+
 module.exports = router;
 
