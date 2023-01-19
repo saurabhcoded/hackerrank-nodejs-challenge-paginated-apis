526a57f3561201807aa5653100cb0dccb55a7c9c
saurabh sharma <saurabhcoded@gmail.com>
2023-01-19 11:53:14 +0530

    done

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,11 +1,23 @@
-var recipes = require('../recipes.json');
-var router = require('express').Router();
+var recipes = require("../recipes.json");
+var router = require("express").Router();
 
-router.get("/?page&limit", function (req, res, next) {
-  const params = req.params;
-  recipes.length=3
-  res.send(recipes)
-})
+// router.get("/", function (req, res, next) {
+//   const params = req.params;
+//   recipes.length=3
+//   res.send(recipes)
+// })
+router.get("/", function (req, res, next) {
+  const { page, limit } = req.query;
+  if ((!page, !limit)) {
+    recipes.length = 3;
+    res.send(recipes);
+  } else {
+    for (let i = 0; i < (page - 1) * 3; i++) {
+      recipes.shift();
+    }
+    recipes.splice(limit);
+    res.send(recipes);
+  }
+});
 
 module.exports = router;
-
